3092570bb61e70ad735433e19e4879812121dc09
Emir <alkalemir@gmail.com>
2022-06-08 15:35:40 +0300

    bug

---
+++ tableView/DetailViewController.swift
@@ -0,0 +1,58 @@
+//
+//  DetailViewController.swift
+//  tableView
+//
+//  Created by Emir Alkal on 8.06.2022.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController, WKNavigationDelegate {
+
+    var webView: WKWebView!
+    var urlString: String?
+    var allowedSites: [String]?
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        webView.allowsBackForwardNavigationGestures = true
+        navigationController?.isToolbarHidden = false
+        
+        view = webView
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadPage(name: urlString!)
+        let refreshButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [refreshButton]
+    }
+    
+    func loadPage(name: String) {
+        let url = URL(string: "https://\(name)")!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for webSite in allowedSites! {
+                if host.contains(webSite) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        
+        let ac = UIAlertController(title: "Error", message: "This site is blocked!", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+        decisionHandler(.cancel)
+    }
+    
+}
--- tableView/ViewController.swift
+++ tableView/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var tableView: UITableView!
+    
+    var datas = ["apple.com", "google.com", "hepsiburada.com", "trendyol.com"]
+    
+    override func loadView() {
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        view = tableView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Sites"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
+    }
+    
+    @objc func addTapped() {
+        let ac = UIAlertController(title: "Add Site", message: "Enter url like example.com", preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { action in
+            
+            if ac.textFields![0].text! != "" {
+                self.datas.append(ac.textFields![0].text!)
+                self.tableView.reloadData()
+            }
+        }))
+        
+        present(ac, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = datas[indexPath.row]
+        return cell
     }
 
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let destinationVC = DetailViewController()
+        
+        destinationVC.allowedSites = datas
+        destinationVC.urlString = datas[indexPath.row]
+        navigationController?.pushViewController(destinationVC, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            datas.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
 }
 
