9e9dc459e20239fd6e18eba012295c8675f4fb8e
Emir <alkalemir@gmail.com>
2022-06-08 14:33:55 +0300

    Initial Commit

---
+++ tableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by Emir Alkal on 8.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
